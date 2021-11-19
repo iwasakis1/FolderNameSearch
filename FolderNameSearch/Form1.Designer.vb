f47b24f603725e77d2101f72f28d75c8d564f9b8
chu arai <iwasaki@daisosystem.co.jp>
2021-11-19 17:30:43 +0900

    プロジェクト ファイルを追加します。

---
+++ FolderNameSearch/Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(12, 241)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(130, 75)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(536, 208)
+        Me.ListBox1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.TextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(654, 19)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(678, 297)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FolderNameSearch/Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports System.IO
+Imports System.Collections.Generic
+Imports System.Runtime.InteropServices
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    <DllImport("kernel32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function FindFirstFile(ByVal lpFileName As String, ByRef lpFindFileData As WIN32_FIND_DATA) As IntPtr
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        TextBox1.Text = ""
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+
+        Dim ds = Directory.EnumerateDirectories(TextBox1.Text, "*", SearchOption.TopDirectoryOnly).ToArray
+
+        mainl(ds)
+
+
+        'Directory.GetDirectories(TextBox1.Text, "*", SearchOption.AllDirectories)
+        Label1.Text = "検索終了"
+
+    End Sub
+
+    Private Sub mainl(ByVal ss() As String)
+        Dim atta = False
+        For Each s In ss
+            If SkipCheck(s) Then Continue For
+            Dim bs = backupchk(s)
+            If bs <> "" Then
+                atta = True
+                Exit For
+            End If
+        Next
+
+        If atta Then
+            For Each s In ss
+                If SkipCheck(s) Then Continue For
+                If chekstr(s) Then
+                    ListBox1.Items.Add(s)
+                    ListBox1.SelectedIndex = ListBox1.Items.Count - 1
+                End If
+            Next
+        Else
+            For Each s In ss
+                If SkipCheck(s) Then Continue For
+                Try
+                    Dim ds = Directory.EnumerateDirectories(s, "*", SearchOption.TopDirectoryOnly).ToArray
+                    mainl(ds)
+                Catch ex As Exception
+                End Try
+            Next
+        End If
+    End Sub
+
+    Private Function chekstr(ByVal s As String) As Boolean
+        If (Regex.IsMatch(s, $"{TextBox2.Text}", RegexOptions.IgnoreCase)) Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    Private Function SkipCheck(ByVal s As String) As Boolean
+        'Debug.WriteLine(s)
+        Label1.Text = s
+        Application.DoEvents()
+
+        If (Regex.IsMatch(s, ".git|.vs|obj|bin|my project", RegexOptions.IgnoreCase)) Then
+            Return True
+        End If
+        Return False
+    End Function
+
+
+    Private Function backupchk(ByVal ss As String) As String
+        Dim fs = IO.Path.GetFileName(ss)
+        If fs.IndexOf("backup", StringComparison.CurrentCultureIgnoreCase) >= 0 Then
+            Return ss
+        End If
+        Return ""
+    End Function
+
+
+
+    ' The CharSet must match the CharSet of the corresponding PInvoke signature
+    <StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Auto)>
+    Structure WIN32_FIND_DATA
+        Public dwFileAttributes As UInteger
+        Public ftCreationTime As System.Runtime.InteropServices.ComTypes.FILETIME
+        Public ftLastAccessTime As System.Runtime.InteropServices.ComTypes.FILETIME
+        Public ftLastWriteTime As System.Runtime.InteropServices.ComTypes.FILETIME
+        Public nFileSizeHigh As UInteger
+        Public nFileSizeLow As UInteger
+        Public dwReserved0 As UInteger
+        Public dwReserved1 As UInteger
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=260)> Public cFileName As String
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=14)> Public cAlternateFileName As String
+    End Structure
+
+End Class
